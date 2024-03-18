c4039446343520fe0d3f8e53a215d09eeffc5ea3
nihad <nihad@hodo.in>
2024-03-18 21:37:19 +0530

    new changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="shadow">
+        <div className="max-w-5xl mx-auto h-14 p-3 flex items-center justify-between font-medium">
+            <Link href="/">New Meeting</Link>
+            <SignedIn>
+                <div className="flex items-center gap-5">
+                    <Link href="/meetings">Meetings</Link>
+                    <UserButton />
+                </div>
+            </SignedIn>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+        </div>
+    </header>
+  )
+}
